912a7024ceb360c5b90fed34d191ab4e85ec42cc
mayank <mayank.reg@gmail.com>
2023-04-20 19:07:33 +0530

    movies app using react redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { createContext } from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+import App from "./components/App";
+import "./index.css";
+import rootReducer from "./reducers";
+
+// dispatch & getState are not store objects
+// curriable form of logger(obj, next, action)
+  // logger(obj)(next)(action)
+  // next refers to...next middleware in chain or dispatch function at last MW
+
+// const logger = function ({dispatch, getState}){
+//   return function(next){
+//     return function(action){
+//       if (typeof action !== "function") {
+//         console.log("ACTION TYPE = ", action.type);
+//       }
+//       next(action);
+//     }
+//   }
+// }
+
+// var f = () => () => () => {return 1}
+  // f()()()
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+    if (typeof action !== "function") {
+      console.log("ACTION TYPE = ", action.type);
+    }
+    next(action);
+  };
+
+// const thunk =
+//   ({ dispatch, getState }) =>
+//   (next) =>
+//   (action) => {
+//     if (typeof action === "function") {
+//       action(dispatch);
+//       return;
+//     }
+//     next(action);
+//   };
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+
+export const StoreContext = createContext();
+
+class Provider extends React.Component {
+  render() {
+    const { store } = this.props;
+    return (
+      <StoreContext.Provider value={store}>
+        {this.props.children}
+      </StoreContext.Provider>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
